ed0515cd78dbc4eaa9a0dadddbabed2e5165fdd6
Sean Huang <hyprul@ucla.edu>
2019-11-27 16:58:51 -0600

    food

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <h1 className="heading" spellCheck="false">
+      My Favourite Foods
+    </h1>
+    <div>
+      <img
+        src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/hand-held-bbq-favorites-royalty-free-image-694189032-1564779029.jpg?resize=480:*"
+        alt="food"
+      />
+      <img
+        src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/hand-held-bbq-favorites-royalty-free-image-694189032-1564779029.jpg?resize=480:*"
+        alt="food"
+      />
+      <img
+        src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/hand-held-bbq-favorites-royalty-free-image-694189032-1564779029.jpg?resize=480:*"
+        alt="food"
+      />
+    </div>
   </div>,
   document.getElementById("root")
 );
