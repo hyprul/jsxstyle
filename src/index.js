3eba179d202f94275b8659266ceca7169c2bc223
Sean Huang <hyprul@ucla.edu>
2019-11-28 17:19:07 -0600

    lorem picsum

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const img = "https://picsum.photos/id/874/200/200";
+
 ReactDOM.render(
   <div>
     <h1 className="heading" spellCheck="false">
       My Favourite Foods
     </h1>
     <div>
-      <img
+      <img src={img} />
+      {/* <img
         src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/hand-held-bbq-favorites-royalty-free-image-694189032-1564779029.jpg?resize=480:*"
         alt="food"
       />
@@ -18,7 +21,7 @@ ReactDOM.render(
       <img
         src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/hand-held-bbq-favorites-royalty-free-image-694189032-1564779029.jpg?resize=480:*"
         alt="food"
-      />
+      /> */}
     </div>
   </div>,
   document.getElementById("root")
